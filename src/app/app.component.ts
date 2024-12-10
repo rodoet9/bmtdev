b8679188ce5a719f33cf1fac6ecb4cb5ea94109c
Teodor Badarcea <teodorbadarcea@bearingpoint.com>
2024-12-10 15:24:30 +0200

    deleted layout component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { LayoutComponent } from '../common-components/layout/layout.component';
+import { HeaderComponent } from '../common-components/header/header.component';
+import { FooterComponent } from '../common-components/footer/footer.component';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
 	selector: 'app-root',
-	imports: [LayoutComponent],
+	imports: [HeaderComponent, FooterComponent, RouterOutlet, RouterModule],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
+import { PortfolioComponent } from '../portfolio/portfolio.component';
+import { ServicesComponent } from '../services/services.component';
+import { ContactComponent } from '../contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	// { path: '', component: HomeComponent },
+	{ path: 'home', component: HomeComponent },
+	{ path: 'services', component: ServicesComponent },
+	{ path: 'portfolio', component: PortfolioComponent },
+	{ path: 'contact-us', component: ContactComponent },
+];
