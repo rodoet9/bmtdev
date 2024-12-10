8489c1cb1aef0ea7c5d70e7a22738b2f4def3749
Teodor Badarcea <teodorbadarcea@bearingpoint.com>
2024-12-10 11:02:29 +0200

    created general layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LayoutComponent } from '../common-components/layout/layout.component';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: 'app-root',
+	imports: [LayoutComponent],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'bmtdev';
+	title = 'bmtdev';
 }
--- src/common-components/layout/layout.component.ts
+++ src/common-components/layout/layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
-  selector: 'app-layout',
-  imports: [],
-  templateUrl: './layout.component.html',
-  styleUrl: './layout.component.scss'
+	selector: 'app-layout',
+	imports: [HeaderComponent, FooterComponent, RouterModule],
+	templateUrl: './layout.component.html',
+	styleUrl: './layout.component.scss',
 })
-export class LayoutComponent {
-
-}
+export class LayoutComponent {}
