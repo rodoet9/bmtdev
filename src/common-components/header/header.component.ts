a507c6c13897e07d241259757549ee9649b3a782
Teodor Badarcea <teodorbadarcea@bearingpoint.com>
2024-12-13 17:00:51 +0200

    @media 375px, font

--- src/common-components/header/header.component.ts
+++ src/common-components/header/header.component.ts
@@ -1,10 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
 	selector: 'app-header',
-	imports: [RouterLink, RouterModule],
+	imports: [RouterLink, RouterModule, CommonModule],
 	templateUrl: './header.component.html',
 	styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+	menuOpen = false;
+
+	toggleMenu() {
+		this.menuOpen = !this.menuOpen;
+	}
+}
